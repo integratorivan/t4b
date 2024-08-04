09d92b425682bc3c2aaf2e8c5d7f5759a2ff1452
boss <akwigliz@icloud.com>
2024-08-04 15:35:03 +0300

    [feat] refactoring

--- app/(modules)/events/event-button/index.tsx
+++ app/(modules)/events/event-button/index.tsx
@@ -2,10 +2,6 @@ import type { FC } from 'react';
 
 import styles from './styles.module.css';
 
-type EventButtonProps = {
-    title: string;
-};
-
 export const EventButton: FC<EventButtonProps> = ({ title }) => {
     return (
         <div className={styles.eventButton}>
